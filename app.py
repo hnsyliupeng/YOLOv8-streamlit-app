1a838b4c1c58330d11091bd7a0474677c0863c43
Liupeng <hao122012@163.com>
2024-05-07 10:14:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,29 +13,35 @@ from PIL import Image
 import streamlit as st
 
 import config
-from utils import load_model, infer_uploaded_image, infer_uploaded_video, infer_uploaded_webcam
+from utils import load_model, load_segmodel,infer_uploaded_image, infer_uploaded_video, infer_uploaded_webcam
 
 # setting page layout
 st.set_page_config(
-    page_title="Interactive Interface for YOLOv8",
+    page_title="Fiber Detection for YOLOv8",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
     )
 
 # main page heading
-st.title("Interactive Interface for YOLOv8")
+st.title("Fiber Detection and Segmentation for YOLOv8")
 
 # sidebar
-st.sidebar.header("DL Model Config")
+st.sidebar.header("DL Model For Fiber Detection and Segmentation")
 
 # model options
 task_type = st.sidebar.selectbox(
-    "Select Task",
+    "Select Detection model",
     ["Detection"]
+)
+# model options
+seg_type = st.sidebar.selectbox(
+    "Select Segmentation model",
+    ["Segmentation"]
 )
 
 model_type = None
+segmodel_type= None
 if task_type == "Detection":
     model_type = st.sidebar.selectbox(
         "Select Model",
@@ -43,6 +49,14 @@ if task_type == "Detection":
     )
 else:
     st.error("Currently only 'Detection' function is implemented")
+
+if seg_type == "Segmentation":
+    segmodel_type = st.sidebar.selectbox(
+        "Segment model",
+        config.Segment_MODEL_LIST
+    )
+else:
+    st.error("Currently only 'Segmentation' function is implemented")
 
 confidence = float(st.sidebar.slider(
     "Select Model Confidence", 30, 100, 50)) / 100
@@ -59,6 +73,18 @@ try:
 except Exception as e:
     st.error(f"Unable to load model. Please check the specified path: {model_path}")
 
+segmodel_path = ""
+if segmodel_type:
+    segmodel_path = Path(config.Segment_MODEL_DIR, str(segmodel_type))
+else:
+    st.error("Please Select Model in Sidebar")
+
+# load pretrained DL model
+try:
+    segmodel = load_segmodel(segmodel_path)
+except Exception as e:
+    st.error(f"Unable to load model. Please check the specified path: {segmodel_path}")
+
 # image/video options
 st.sidebar.header("Image/Video Config")
 source_selectbox = st.sidebar.selectbox(
@@ -68,7 +94,7 @@ source_selectbox = st.sidebar.selectbox(
 
 source_img = None
 if source_selectbox == config.SOURCES_LIST[0]: # Image
-    infer_uploaded_image(confidence, model)
+    infer_uploaded_image(confidence, model,segmodel_path)
 elif source_selectbox == config.SOURCES_LIST[1]: # Video
     infer_uploaded_video(confidence, model)
 elif source_selectbox == config.SOURCES_LIST[2]: # Webcam
--- utils.py
+++ utils.py
@@ -9,11 +9,14 @@
 -------------------------------------------------
 """
 from ultralytics import YOLO
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 import streamlit as st
 import cv2
+import torch
 from PIL import Image
 import tempfile
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def _display_detected_frames(conf, model, st_frame, image):
     """
@@ -53,8 +56,18 @@ def load_model(model_path):
     model = YOLO(model_path)
     return model
 
+def load_segmodel(segmodel_path):
+    """
+    Loads a SAM object segmentation model from the specified model_path.
 
-def infer_uploaded_image(conf, model):
+    Parameters:
+        model_path (str): The path to the YOLO model file.
+
+    Returns:
+        A SAM object segmentation model.
+    """
+    return segmodel_path
+def infer_uploaded_image(conf, model,segmodel_path):
     """
     Execute inference for uploaded image
     :param conf: Confidence of YOLOv8 model
@@ -84,8 +97,15 @@ def infer_uploaded_image(conf, model):
                 res = model.predict(uploaded_image,
                                     conf=conf)
                 boxes = res[0].boxes
+                for result in res:
+                    boxex = result.boxes  # Boxes object for bbox outputs
+    
+                bbox = boxex.xyxy.tolist()
+                bbox = [[int(i) for i in box] for box in bbox]
+                Ts = res[0].boxes.xyxy                      
+                numfiber=str(Ts.shape[0])
                 res_plotted = res[0].plot()[:, :, ::-1]
-
+                image=uploaded_image
                 with col2:
                     st.image(res_plotted,
                              caption="Detected Image",
@@ -94,6 +114,15 @@ def infer_uploaded_image(conf, model):
                         with st.expander("Detection Results"):
                             for box in boxes:
                                 st.write(box.xywh)
+                         
+                        with st.container():
+                             st.write("The number of fiber is")
+                             st.write(numfiber)
+                        with st.container():
+                             st.write("Segmentation")
+                            # st.write(source_img)
+                             #st.write(image)
+                             segsamplot(source_img,bbox,segmodel_path)
                     except Exception as ex:
                         st.write("No image is uploaded yet!")
                         st.write(ex)
@@ -164,3 +193,120 @@ def infer_uploaded_webcam(conf, model):
                 break
     except Exception as e:
         st.error(f"Error loading video: {str(e)}")
+
+# Plot segmentation 
+def segsamplot(source_img,bbox,segmodel_path):
+
+  sam_checkpoint = segmodel_path
+  #model_type = "vit_h"
+  model_type = "vit_b"
+  device = "cpu"
+
+  sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+  sam.to(device=device)
+  #image = cv2.resize(image, (720, int(720 * (9 / 16))))
+  img = Image.open(source_img)
+  #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  image = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
+  #image = cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2RGB)
+  predictor = SamPredictor(sam)
+  predictor.set_image(image)
+  #bbox = boxes.xyxy.tolist()
+ # bbox = [[int(i) for i in box] for box in bbox]
+  input_boxes = torch.tensor(bbox, device=predictor.device)
+
+  transformed_boxes = predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
+  masks, _, _ = predictor.predict_torch(
+      point_coords=None,
+      point_labels=None,
+      boxes=transformed_boxes,
+      multimask_output=False,
+  )
+
+  for i, mask in enumerate(masks):
+
+      binary_mask = masks[i].squeeze().numpy().astype(np.uint8)
+
+      # Find the contours of the mask
+      contours, hierarchy = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+      largest_contour = max(contours, key=cv2.contourArea)
+
+      # Get the new bounding box
+      bbox = [int(x) for x in cv2.boundingRect(largest_contour)]
+
+      # Get the segmentation mask for object 
+      segmentation = largest_contour.flatten().tolist()
+
+      # Write bounding boxes to file in YOLO format
+      with open('BBOX_yolo.txt', 'w') as f:
+          for contour in contours:
+              # Get the bounding box coordinates of the contour
+              x, y, w, h = cv2.boundingRect(contour)
+              # Convert the coordinates to YOLO format and write to file
+              f.write('0 {:.6f} {:.6f} {:.6f} {:.6f}\n'.format((x+w/2)/image.shape[1], (y+h/2)/image.shape[0], w/image.shape[1], h/image.shape[0]))
+              f.write('\n')
+      mask=segmentation
+      
+          # load the image
+      #width, height = image_path.size
+      #img = Image.open(source_img)
+      width, height = img.size
+
+      # convert mask to numpy array of shape (N,2)
+      mask = np.array(mask).reshape(-1,2)
+
+      # normalize the pixel coordinates
+      mask_norm = mask / np.array([width, height])
+
+      # compute the bounding box
+      xmin, ymin = mask_norm.min(axis=0)
+      xmax, ymax = mask_norm.max(axis=0)
+      bbox_norm = np.array([xmin, ymin, xmax, ymax])
+
+      # concatenate bbox and mask to obtain YOLO format
+      yolo = np.concatenate([bbox_norm, mask_norm.reshape(-1)])
+
+      # compute the bounding box
+      # write the yolo values to a text file
+      with open('yolomask_format.txt', 'w') as f:
+          for val in yolo:
+              f.write("{:.6f} ".format(val))
+          f.write('\n')
+
+      # Print the bounding box and segmentation mask
+      print("Bounding box:", bbox)
+      #print("Segmentation mask:", segmentation)
+      print("yolo",yolo)
+  #plt.figure(figsize=(10, 10))
+  fig, ax = plt.subplots()
+  #with st.image(image):
+  im = ax.imshow(image)
+  for mask in masks:
+     show_mask(mask.cpu().numpy(), plt.gca(), random_color=True)
+  for box in input_boxes:
+     show_box(box.cpu().numpy(), plt.gca())
+  plt.axis('off')
+  st.pyplot(fig) 
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+   # ax.imshow(mask_image)
+    ax.imshow(mask_image)
+    #st.pyplot()
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))   
